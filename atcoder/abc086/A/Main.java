22d048509934f5075c473a6b107868a0d50bb771
Wataru <kisarailgun@gmail.com>
2020-11-19 14:42:54 +0900

    practice abc085-b

--- atcoder/abc086/A/Main.java
+++ atcoder/abc086/A/Main.java
@@ -10,7 +10,6 @@ class Main {
         final Scanner sc = new Scanner(System.in);
         long a;
         a = sc.nextLong();
-        System.out.println(a);
         long b;
         b = sc.nextLong();
         solve(a, b);
